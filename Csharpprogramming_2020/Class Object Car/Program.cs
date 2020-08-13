5d618e6fc83641abe835f503f9b3ac7be0715456
102932079 <61527439+102932079@users.noreply.github.com>
2020-08-13 15:43:13 +1000

    update

---
+++ Csharpprogramming_2020/Class Object Car/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+//A variable is nothing but a name given to a storage area that our programs can manipulate. Each variable in C# has a specific type, which determines the size and layout of the variable's memory the range of values that can be stored within that memory and the set of operations that can be applied to the variable.
+// object can contain another object c1.en1.NumCylinders
+namespace Class_Object_Car
+{
+    // between curly braces (bracket) called program box (the class should be always start with capital)
+    //after the construtors and method we now can start to create the car(instantiate)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine e1 = new Engine(4, 500);
+            
+            Car c2 = new Car ("ZXC987", 8, 0, 0, e1);
+            
+            Car c3 = new Car ("ABC123", 5, 0, 0, 6, 300);
+
+            System.Console.WriteLine(c3.en.NumCylinders);
+            System.Console.WriteLine(c2.en.NumCylinders);
+
+
+            // instantiate a new car - look at variable declear the type and give it values
+            //type is car new keyword does the instantiating (create a new defalut car )
+            //after keyword new we call the construtor (construoter define values)
+            Car c1 = new Car();
+            // it will broken when insert engine with car beacuse no construtors renference
+            //Car c2 = new Car( "ABC123", 5 , 0, 0, 0);
+
+            Car c2 = new Car( "ABC123", 5 , 0, 0, 0, 6, 300);
+
+            //use c2 to locate the car object then use . to goes to the next level(attribute and method) into the box next layout
+            // int dot menu attribute blue messageer purple
+
+            System.Console.WriteLine($"Before:" + c2.Passengers);
+
+            c2.UpdatePassengers(5);
+
+            System.Console.WriteLine($"after:" + c2.Passengers);
+
+            System.Console.WriteLine(c1.Passengers);
+
+            c2.UpdatePassengers(-3);
+
+            System.Console.WriteLine($"after:" + c2.Passengers);
+
+            c2.UpdatePassengers(-6);
+
+            System.Console.WriteLine($"after:" + c2.Passengers);
+
+            
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+   
+
+    // object can contain another object c1.en1.NumCylinders
+    //defalut string will be null or blank defarlut int will be 0
+     class Engine
+     {
+         //attribute
+         public int NumCylinders;
+         public int CylinderSize;
+
+         //create defalut constutor and input construtor
+         //also we can use the light bulb next to class to generate constutor automatily with vscode system
+
+
+         public Engine ()
+         {
+             this.NumCylinders = 0;
+             this.CylinderSize = 0;
+             
+         }
+         public Engine (int nc, int cs)
+         {
+             this.NumCylinders = nc;
+             this.CylinderSize = cs;
+
+         }
+
+
+         public void Rev()
+         {
+             System.Console.WriteLine("Vroom Vroom");
+         }
+
+     }
+
+
+
+
+    //-----------------------------------------------------------------------------------------------
+
+
+    public class Car
+    // do first define attributes what you what those thing do in the system - modelling what car can do
+    //the references below is from construtors
+
+    // when we create a class just put public for every class attrribute method you created for accessbility
+    {
+        public string Rego;
+        public int PassengerLimit;
+        public int Passengers;
+        public int Xcord;
+        public int Ycord;
+
+        //make other object into an attribute engine to car
+        public Engine en;
+
+        //=============================================================================================================
+        //do third add constructors -part of class allow it to create object (will be always public beacuse it will be called by external code)
+        //the name of constructors will be the always the class case sensitive
+        //defalut constructors beacuse of there is nothing in parmeter list
+        //in the constrctor we initiate the defalut as well (determine what are those values are)
+        //those initiate below are referencing the attribute above
+        //this key word mean the attributre from this(car) class
+        //beacuse i can create int Rego ; Rego = 10; but it's different from this.rego
+        
+        public Car()
+        {
+            this.Rego = "";
+            this.PassengerLimit = 0;
+            this.Passengers = 0;
+            this.Xcord = 0;
+            this.Ycord = 0;
+            this.en = null;
+        }
+
+        //it could has mutiple construtors, provide more parameter with values and provide information to the method
+        //the construtor above is for set up defalut the constutor below is for ask the values pass into the each of attribute
+        /*
+        public Car(string rego, int pl , int pass, int x, int y, Engine e)
+        {
+            this.Rego = rego;
+            this.PassengerLimit = pl;
+            this.Passengers = pass;
+            this.Xcord = x;
+            this.Ycord = y;
+            this.en = e;
+
+        }
+        */
+        //threre are two way of insert engine to car as a whole or all the attribe from engine
+        public Car(string rego, int pl , int pass, int x, int y, int nc, int cs)
+        {
+            this.Rego = rego;
+            this.PassengerLimit = pl;
+            this.Passengers = pass;
+            this.Xcord = x;
+            this.Ycord = y;
+            this.en = new Engine(nc, cs);
+
+        }
+
+
+
+        //===============================================================================================================
+
+        // do senond oprations method the return type is the object itself
+
+        ///<summary>
+        ///Adds number of newPassengers to passengers. to remove passengers provide negatove newPassengers values
+        ///</summary>
+        ///<param name="newPassengers"></param>
+        // those oprators
+        //c += a ---- c = c + a
+    
+        public void UpdatePassengers(int newPassengers)
+        {
+            // check to not exeed the passenger limit and reasonable
+            /*
+            if ( this.Passengers + newPassengers >= 0 )
+            {
+                if ( this.Passengers + newPassengers <= this.PassengerLimit )
+                {
+                this.Passengers += newPassengers;
+                }
+            }
+            */
+            // && and ||or
+            if (this.Passengers + newPassengers <= this.PassengerLimit && this.Passengers + newPassengers >= 0 )
+            {
+                this.Passengers += newPassengers;
+            }
+            
+            
+            
+        }
+
+        public void Move(int newX, int newY)
+        {
+            this.Xcord = newX;
+            this.Ycord = newY;
+
+        }
+
+        
+
+
+    }
+}
---
+++ Csharpprogramming_2020/DD/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //create variable new is the key word to instantiate 
+        {
+            Die d1 = new Die();// use a default constrator
+            Die d2 = new Die(12);
+            Die d3 = new Die(20);
+
+            List<Die> diceList = new List<Die>();
+            diceList1.Add(d1);
+            diceList2.Add(d2);
+            diceList3.Add(d3);
+
+            List<Die> diceList2 = new List<Die>();
+            diceList2.Add(d2);
+
+            //create few people
+            Person p1 = new Person("William Wallace", diceList1);
+            Person p2 = new Person("Joan of Arc", diceList2);
+
+            //p1 is the object .(dot) is the symbol that into that object will show a list of accessable
+
+            System.Console.WriteLine(p1.Name);
+            System.Console.WriteLine(p1.Dice.Count);
+
+            System.Console.WriteLine(p2.Name);
+            System.Console.WriteLine(p2.Dice.Count);
+
+            p2.AddNewDie(18);
+            System.Console.WriteLine(p2.Dice.Count);
+
+            // do sth 20 time with for loop
+
+            for(int i=0; i < 20; i++)
+            {
+                System.Console.WriteLine(d1.Roll());
+                System.Console.WriteLine(d2.Roll());
+            }
+
+            for(int i=0; i < 20; i++)
+            {
+                //roll William Wallace's second die
+                System.Console.WriteLine(p1.RollDie(1));
+            }
+        }
+    }
+    // the finnal thing to do is check structure before move on
+
+    //the first thing we do is *Create Class* (always public make things accessable)
+    public class Die 
+    // only create all the class in same file for this demo only 
+    // but normally spliting every class in seperate file
+    {
+        public int NumOfSides;
+
+        // the last thing we do is constructors : It’s a special method that return the object from that class Constructor rule the name of constructor is always the same name of the class include casing (case sensitive) instantiate the object
+
+        public Die(int numofsides)
+
+        // what happen in the parameters is passing in the number of size they want for new die that they going to create
+        {
+            //set up the object when we created this to that numofsides,take the integer from numofside to what been asked into that interger
+            this.NumOfSides = numofsides;
+        }
+
+        public Die()
+        {
+            this.NumOfSides = 6;
+        }
+
+
+        // the second thing we do is *Give Method* Description the method is roll ,we dont know how the roll is work yet but we know the return vales is 0
+        public int Roll()
+        {
+            // random is class from default rand is the new object we create we use new to instantiate
+            Random rand = new Random();
+
+            // next is the not negetive random interger and create a variable
+            //purpose create a random object and create a random number and we use it to get a random number between 1 and 7 make those into the variable roll then return the roll
+        
+            int roll = rand.Next(1, (this.NumOfSides + 1));
+
+            return roll; // place holder
+        }
+
+        public class Person 
+        {    
+            public string Name;
+
+                //using function to make list function unlock dice have mutiple die
+                public List<Die> Dice;
+
+                //create constrctor
+
+                public Person(string name, List<Die> dice)
+                {
+                    this.Name = name;
+                    this.Dice = dice;
+                }
+
+                public Person()
+                {
+                    this.Name = "John Doe";
+                    this.Dice = new List<Die>();
+                }
+
+                // The list at the moment is none exist we need to create the list. if die is out of range will return-1
+                ///<summary>
+                ///Rolls a specified die from the Dice list.
+                ///</summary>
+                ///<param name="die">indexing strats at 0</param>
+                ///<returns></returns>
+        
+
+
+                public int RollDie(int die)
+
+                
+                {
+                    int roll = -1;
+                    if(die > 0 && die < Dice.Count)
+                    Dice[die].Roll();
+                    return roll; //place holder
+                }
+                
+
+            ///<summary>
+                ///roll all of the player 's dice once and return the total.
+                ///</summary>
+                ///<param ></param>
+                ///<returns></returns>
+                public int RollAllDice()
+                {
+                        return 0;//place holder
+
+                }
+
+
+                public void AddNewDie(int sides)
+                {
+                    Die d = new Die(sides);
+                    this.Dice.Add(d);
+                }       
+        }
+    }
+}
