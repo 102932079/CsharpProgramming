bd1acbee28017119624f55983ec9b1ad7ba3bfbc
102932079 <61527439+102932079@users.noreply.github.com>
2020-08-24 20:29:25 +1000

    update

---
+++ Csharpprogramming_2020/CIV_UNIT_TEST2-implemation/MiscLib/Group1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace MiscLib
+{
+    public class GroupOneFunctions
+    {
+        //---- Group 1 Functions ----------
+        public int CalcRectArea(int length, int width)
+        {
+            if ((length > 0) & (width > 0))
+            {
+                int RESULT = 2 * (length + width);
+                return RESULT;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool IsFactiorialNum(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                if ((num % i) == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+               
+            }
+             return false;
+        }
+
+        public int CelciusToFarenheit(int celcius)
+        {
+            int Farenheit = ((celcius * 9 / 5) + 32);
+            return Farenheit;
+        }
+
+        public bool IsPalindrome(string word)
+        {
+            string first = word.Substring(0, word.Length / 2);
+            Char[] array = word.ToCharArray(0,word.Length);
+            Array.Reverse(array);
+            string reverseword = array.ToString();
+            string second = reverseword.Substring(0, reverseword.Length / 2);
+            return first.Equals(second);
+
+        }
+
+        public string StringReplace(string word, char replaceChar, char replacement)
+        {
+            Char[] arrays = word.ToCharArray(0,word.Length);
+            if (Array.Exists(arrays, element => element == replaceChar) ==false)
+            {
+                return "The char doesn't exit in the string";
+            }
+            else
+            {
+                arrays[Array.IndexOf(arrays, replaceChar)] = replacement;
+                string newword = arrays.ToString();
+                return newword;
+            }
+        }
+
+        public int CharCount(string word, char countCharacter)
+        {
+            int digitsCount = 0;
+            foreach (char c in word)
+            {
+                if (c==countCharacter)
+                    digitsCount++;
+            }
+            return digitsCount;
+        }
+
+
+    }
+}
---
+++ Csharpprogramming_2020/CIV_UNIT_TEST2-implemation/Tests/Tests.cs
@@ -0,0 +1,90 @@
+using System;
+using Xunit;
+using MiscLib;
+
+namespace Tests
+{
+    public class MiscLibTests
+    {
+        // Write Unit Tests for all functions for your assigned group
+        // Group 1
+
+        // 1. Method receives the two sides of a Rectangle and return the area CalcRectArea(int length, int width) : int
+
+        [Theory]
+        [InlineData(2, 1, 2)]
+        [InlineData(12, 3, 4)]
+               
+        public int CalcRectArea(int expected, int length, int width)
+        {
+            GroupOneFunctions g1 = new GroupOneFunctions(length, width);
+            Assert.Equal(expected, g1.CalcRectArea());
+        }
+
+        // 2. Checks if a number is a factorial IsFactiorialNum(int num) : bool
+
+        [Theory]
+        [InlineData(true, 120)]
+        [InlineData(true, 720)]
+        [InlineData(false, 250)]
+        [InlineData(false, 999)]
+        
+        public bool IsFactiorialNum(bool expected, int num)
+        {
+            GroupOneFunctions g1 = new GroupOneFunctions(num);
+            Assert.Equal(expected, g1.IsFactiorialNum());
+        }
+
+        // 3. Converts a temperature from Celcius to Farenheit, rounded up to the nearest whole number CelciusToFarenheit(int celcius) : int
+
+        [Theory]
+        [InlineData(32, 0)]
+        [InlineData(212, 100)]
+        [InlineData(-148, -100)]
+        public int CelciusToFarenheit(int expected, int celcius)
+        {
+            GroupOneFunctions g1 = new GroupOneFunctions(celcius);
+            Assert.Equal(expected, g1.CelciusToFarenheit());
+        }
+
+        // 4. Checks a string and returns true if it is palindromic IsPalindrome(string word) : bool
+
+        [Theory]
+        [InlineData(true, "abcba")]
+        [InlineData(true, "aaaaa")]
+        [InlineData(false, "abcdef")]
+        [InlineData(false, "abcbc")]
+        
+        public bool IsPalindrome(bool expected, string word)
+        {
+            GroupOneFunctions g1 = new GroupOneFunctions(word);
+            Assert.Equal(expected, g1.IsPalindrome());
+        }
+
+        // 5. Replaces a character when found in a string StringReplace(string word, char replaceChar, char replacement) : string
+
+        [Theory]
+        [InlineData("dbc", "abc", "a", "d")]
+        [InlineData("oyz", "xyz", "x", "o")]    
+        [InlineData("?", "abc" , "m", "n")]
+        public string StringReplace(string expected, string word, char replaceChar, char replacement)
+        {
+            GroupOneFunctions g1 = new GroupOneFunctions(word, replaceChar, replacement);
+            Assert.Equal(expected, g1.StringReplace());
+        }
+
+        // 6. Counts the number of times a character occurs in a string CharCount(string word, char countCharacter) : int
+        
+        [Theory]
+        [InlineData(5, "aaaaa", "a")]
+        [InlineData(0, "zzzzz", "a")]    
+        
+        public int CharCount(int expected, string word, char countCharacter)
+        {
+            GroupOneFunctions g1 = new GroupOneFunctions(word, countCharacter);
+            Assert.Equal(expected, g1.CharCount());
+        } 
+
+    
+    }
+}
