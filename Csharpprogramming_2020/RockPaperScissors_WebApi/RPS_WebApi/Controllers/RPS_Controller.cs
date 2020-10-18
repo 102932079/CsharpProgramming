cac7f03adc3ff0fab5259ea06bdf7d38b6c11cb9
102932079 <61527439+102932079@users.noreply.github.com>
2020-10-18 17:14:53 +1100

    ready

---
+++ Csharpprogramming_2020/RockPaperScissors_WebApi/RPS_WebApi/Controllers/RPS_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RPS_WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RPS_Controller : ControllerBase
+    {
+        
+        
+    }
+}
