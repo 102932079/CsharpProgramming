441e5b64bbd6506014db0aa8ff219cd967d6b843
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-14 14:33:11 +1100

    add microsoft guide link

--- DBconnectExample/Controllers/DBConnectionTest.cs
+++ DBconnectExample/Controllers/DBConnectionTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//find compelete guide from microsoft document
+//https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlconnection?view=dotnet-plat-ext-5.0
+using System;
 
 namespace Controllers
 {
@@ -8,7 +10,7 @@ namespace Controllers
     {
         [HttpGet]
         public string TestConnection(){
-            
+
         }
     }
 }
