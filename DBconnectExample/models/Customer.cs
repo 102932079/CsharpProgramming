5752cdb31ea3b210d569496e980dcba0b92d4295
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-14 18:40:19 +1100

    create class customer

---
+++ DBconnectExample/models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBCONNECTEXAMPLE.models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string surname { get; set; }
+        
+    }
+}
