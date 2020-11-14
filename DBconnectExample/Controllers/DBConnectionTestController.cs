118018ca061ae9775f3ca89f0f2f752f29cfcea3
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-14 18:05:52 +1100

    add nuget package manager

---
+++ DBconnectExample/Controllers/DBConnectionTestController.cs
@@ -0,0 +1,29 @@
+﻿//find compelete guide from microsoft document
+//https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlconnection?view=dotnet-plat-ext-5.0
+//or
+//https://www.guru99.com/c-sharp-access-database.html
+
+using System;
+using Microsoft.AspNetCore.Mvc;//for db -re 
+using System.Data.SqlClient; //sqlconnection
+
+//we need sql connection string and read block
+
+//namespace issue
+namespace DBCONNECTEXAMPLE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DBConnectionTestController
+    {
+        [HttpGet]
+        public string TestConnection(){
+            //not reconise connection dont have the libery try (dotnet restore)
+            //ctrl + shift P console runner NuGet Package Manager: add package
+            //need to download Nuget Package Manager in extension
+            //system.data.sqlclient
+            SqlConnection conn = new SqlConnection();
+            SqlConnection cnn;
+        }
+    }
+}
